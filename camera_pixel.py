b1445d40790b5d56a47c073c36f38310695c9aa1
punchnnp <punch.nunnapat@gmail.com>
2021-04-07 21:06:44 +0700

    get pixel arrays in every 10 frame

--- camera_pixel.py
+++ camera_pixel.py
@@ -11,16 +11,21 @@ print("MLX addr detected on I2C", [hex(i) for i in mlx.serial_number])
 mlx.refresh_rate = adafruit_mlx90640.RefreshRate.REFRESH_2_HZ
 
 frame = [0] * 768
+x = 0
 while True:
+    x += 1
     try:
         mlx.getFrame(frame)
-    except ValueError:
-        # these happen, no biggie - retry
-        continue
+        if (x % 10) == 0:
+            with open(f"image_nopp{x % 10}.txt")as f:
+                for h in range(24):
+                    for w in range(32):
+                        t = frame[h * 32 + w]
+                        f.write(t)
+                    f.write("\n")
 
-    for h in range(24):
-        for w in range(32):
-            t = frame[h*32 + w]
-            print("%0.1f, " % t, end="")
-        print()
-    print()
+    except ValueError:
+       # these happen, no biggie - retry
+      continue
+
+    print(frame)
