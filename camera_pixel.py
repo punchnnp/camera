ab20d1e3ca605e4d8aa5fc188789c600ba9c771a
punchnnp <punch.nunnapat@gmail.com>
2021-04-07 22:06:01 +0700

    pixel in 1 line

--- camera_pixel.py
+++ camera_pixel.py
@@ -9,21 +9,21 @@ print("MLX addr detected on I2C", [hex(i) for i in mlx.serial_number])
 
 mlx.refresh_rate = adafruit_mlx90640.RefreshRate.REFRESH_2_HZ
 
-frame = [0] * 768
+frame = [0.2] * 768
 x = 0
 while True:
 
     try:
-        mlx.getFrame(frame)
+        # mlx.getFrame(frame)
         x += 1
-        if (x % 10) == 0:
+        if (x / 10) == 0:
             print(frame)
             with open(f"image_hi{x / 10}.txt", "w")as f:
                 for h in range(24):
                     for w in range(32):
                         t = frame[h * 32 + w]
-                        f.write(str(t))
-                    f.write("\n")
+                        f.write("%2f,"%t)
+
 
     except ValueError:
         # these happen, no biggie - retry
