9d9ef62c91f2cbeedb7ef8d4701cd9ad26d4184d
punchnnp <punch.nunnapat@gmail.com>
2021-04-07 22:10:54 +0700

    try convert to photo hi0

--- photo.py
+++ photo.py
@@ -1,2 +1,14 @@
 import matplotlib.pyplot as plt
+image = []
+with open("image_hi0.txt", "r")as f:
+    file = f.read().split(",")
+    for h in range(24):
+        data = []
+        for w in range(32):
+            data.append(float(file[h*32 + w]))
+        image.append(data)
+print(image)
+plt.imshow(image)
+plt.show()
+
 
