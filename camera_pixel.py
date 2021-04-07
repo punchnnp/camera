2f6400832cda70bb6fd9dcf9a21ef08f9d69b7a5
punchnnp <punch.nunnapat@gmail.com>
2021-04-07 22:38:58 +0700

    comment code

--- camera_pixel.py
+++ camera_pixel.py
@@ -1,28 +1,37 @@
+# library for camera
 import board
 import busio
 import adafruit_mlx90640
 
+# make a connection to i2c
 i2c = busio.I2C(board.SCL, board.SDA, frequency=800000)
 
+# connect camera to i2c
 mlx = adafruit_mlx90640.MLX90640(i2c)
 print("MLX addr detected on I2C", [hex(i) for i in mlx.serial_number])
 
+# set camera refresh rate
 mlx.refresh_rate = adafruit_mlx90640.RefreshRate.REFRESH_2_HZ
 
-frame = [0.2] * 768
+# set initial frame [initial value]*length
+frame = [0] * 768
 x = 0
-while True:
+while x <= 10:
 
     try:
-        # mlx.getFrame(frame)
+        mlx.getFrame(frame)
         x += 1
         if (x % 10) == 0:
             print(frame)
+            # open file and save to file
             with open(f"image_hi{x / 10}.txt", "w")as f:
+                # range of horizontal line in array
                 for h in range(24):
+                    # range of vertical line in array
                     for w in range(32):
                         t = frame[h * 32 + w]
-                        f.write("%2f,"%t)
+                        # write pixel to file.txt
+                        f.write("%2f," % t)
 
 
     except ValueError:
--- photo.py
+++ photo.py
@@ -1,14 +1,17 @@
 import matplotlib.pyplot as plt
 image = []
-with open("image_hi0.txt", "r")as f:
+# open file from directory
+with open("image_hi0.0.txt", "r")as f:
+    # separate value into list
     file = f.read().split(",")
+    # make image to 2d array
     for h in range(24):
         data = []
         for w in range(32):
             data.append(float(file[h*32 + w]))
         image.append(data)
+# use imshow matplotlib to show figure
 print(image)
 plt.imshow(image)
 plt.show()
-
 
